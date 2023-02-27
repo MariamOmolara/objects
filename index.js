42138d6c1c517862cee58d42a36680e66237b647
“MariamOmolaragit config --global user.name “MariamOmolara <omolaramariam111@gmail.comgit config --global user.email omolaramariam111@gmail.com>
2023-02-27 21:12:05 +0100

    index.js

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 // Question 1 
 function InstagramPost(
-    HandleOfAuthor,
-    Content,
-    ImageLink,
-    NumberOfView,
-    NumberOfLikes,
+    handleOfAuthor,
+    content,
+    imageLink,
+    numberOfView,
+    numberOfLikes,
 
 ) {
-    this.HandleOfAuthor = HandleOfAuthor;
-    this.Content = Content;
-    this.ImageLink = ImageLink;
-    this.NumberOfView = NumberOfView;
-    this.NumberOfLikes = NumberOfLikes;
+    this.handleOfAuthor = handleOfAuthor;
+    this.content = content;
+    this.imageLink = imageLink;
+    this.numberOfView = numberOfView;
+    this.numberOfLikes = numberOfLikes;
 }
+const myInstagramPost = new InstagramPost ('mariam111', 'I love reading', {}, 1054, 205 )
+console.log(myInstagramPost);
+
+/* Question 2; 
+Create 2 Instagram post objects from the constructor function you created above
+ */
+
+// 1
+const jumaiInstagramPost = new InstagramPost ('Jumai_H', 'Chilling with the girls', 'https://img.freepik.com/free-vector/hand-drawn-flat-design-stack-books_23-2149334862.jpg?w=2000', 5006, 1005 )
+console.log(jumaiInstagramPost);
+// 2
+const tobiInstagramPost = new InstagramPost ('Tobiiii', 'I love to design', {}, 1054, 205 )
+console.log(tobiInstagramPost);
+
+
+
 
 // Question 5
 const presidentialCandidates = {
@@ -34,7 +50,7 @@ for( let party in presidentialCandidates ){
  for ( let key of Object.keys(presidentialCandidates)){
     console.log(key)
     console.log(presidentialCandidates[key])
-    console.log ( presidentialCandidates[party] + "is the presidentila candidates of" + party )
+    console.log(`${presidentialCandidates[key]} is the presidential candidate of ${key}`)
 
  }
 
